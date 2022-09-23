f057e1a85a4a01091d172001606bc6637ba58e14
MercyChebet <chebetm96@gmail.com>
2022-09-23 13:33:59 +0300

    COmpleted Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RockyMountain from './parks/RockyMountain';
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(RockyMountain.trees);
 
-  return <h1>Colorado State Parks!</h1>;
+return (
+  <MesaVerde />
+  
+)
+
+
 }
+export default ColoradoStateParks
